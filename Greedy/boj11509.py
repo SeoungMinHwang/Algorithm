049694c1600460eb865d08a7bc25d09c319b4635
SeoungMinHwang <117965731+SeoungMinHwang@users.noreply.github.com>
2023-04-22 11:27:40 +0900

    boj11509 (풍선 맞추기)

---
+++ Greedy/boj11509.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/11509
+# 공간을 사용해서 시간을 버는것이 핵심
+
+
+from collections import deque
+
+n = int(input())
+balloons = list(map(int, input().split()))
+
+check = [0] * (n+1)
+answer = 0
+
+
+for i in range(n):
+    if check[balloons[i]] == 0:
+        answer += 1
+        check[balloons[i]-1] += 1
+        
+    else:
+        check[balloons[i]] -= 1
+        check[balloons[i]-1] += 1
+print(answer)
+            
